1d3bda0d835489c3b464bd3ad47cbca0aacfdbb1
Roger Wells <rsw359@gmail.com>
2024-06-14 07:46:56 +0900

    product and card

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,13 +5,13 @@
  <header class="flex flex-col relative z-20">
     <div class="max-v-[1400] mx-auto w-full flex items-center  justify-between p-4 py-6">
        <a href="/">
-        <h1 class="font-semibold">Gym<span class="text-fuchsia-700">Helper</span></h1>
+        <h1 class="font-semibold">Gym<span class="text-indigo-400">Helper</span></h1>
        </a>
        <button class="md:hidden grid place-items-center">
          <i class="fa-solid fa-bars"></i>
        </button>
        <nav class="hidden md:flex items-center gap-4　lg:gap-6">
-         <a class="duration-200 hover:text-indigo-500 cursor-pointer" href="#products">Products</a>
+         <a class="duration-200 hover:text-indigo-500 cursor-pointer" href="#product">Product</a>
          <a class="duration-200 hover:text-indigo-500 cursor-pointer" href="#reviews">Reviews</a>
          <a class="duration-200 hover:text-indigo-500 cursor-pointer" href="#faq">FAQ</a>
          <button class="specialBtn">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import SectionWrapper from "../components/SectionWrapper.svelte";
 	import Hero from "../components/Hero.svelte";
+	import Product from "../components/Product.svelte";
 
 </script>
 <main class="flex flex-col">
  <Hero/>
+ <Product/>
 
 </main>
