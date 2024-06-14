97e45a2fc838b681ed01f83a95be5d8d43fb6d56
Roger Wells <rsw359@gmail.com>
2024-06-14 10:27:41 +0900

    product images and ordering

--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -5,7 +5,7 @@
 
 <div class="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap:14 lg:gap-20 relative text-base  ">
   
-  　<div class="flex flex-col gap-8 md:gap-10"> 
+  　<div class={"flex flex-col gap-8 md:gap-10" + (index % 2 === 1 ? ' order-2 ' : '' )}> 
     <h4 class="text-2xl sm:text-3xl md:text-4xl max-w-[1000px] w-full font-medium relative pr-10 after:absolute after:top-full after:left-0 after:w-1/5 after:h-1.5 after:mt-1 after:bg-slate-900">
     <slot/>
     </h4>
@@ -20,6 +20,20 @@
         </div>
       {/each}
     </div>
-</div>
-
+    <div class="flex items-center">
+      <button class="specialBtnDark mr-auto font-semibold">
+        <p>Try free today &rarr;</p>
+      </button>
+    </div>
   </div>
+  <div class={"flex flex-col dropShadow overflow-hidden rounded-b-lg"}>
+  <div class="rounded-t-xl h-8 sm:h-10 bg-white opacity-60 px-3 flex items-center gap-2">
+    {#each [1,2,3] as i }
+    <div class="rounded-full aspect-square w-2.5 sm:w-3 bg-indigo-300"/>
+    {/each }
+  </div>
+  <div class="flex flex-col bg-white gap-4 flex-1">
+    <img src={productFeature.imgUrl} alt="">
+  </div>
+  </div>
+</div> 
